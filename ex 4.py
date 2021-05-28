9c7007330ff08bbf19cca737cc91e1679b6dd002
lraphaelcs <lucas.raphael@aluno.ufop.edu.br>
2021-05-28 13:56:50 -0300

    Create ex 4.py
    Apostila São Carlos
    Exercícios Lista

---
+++ ex 4.py
@@ -0,0 +1,9 @@
+L = [5, 7, 2, 9, 4, 1, 3]
+print("a) Tamanho da lista: ", len(L))
+print("b) Maior valor da lista: ", max(L))
+print("c) Menor valor da lista: ",min(L))
+print("d) Soma dos valores da lista: ",sum(L))
+L.sort()
+print("e) Lista em ordem crescente: ", L)
+L.reverse()
+print("f) Lista em ordem decrescente: ", L)
