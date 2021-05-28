20138313b189ea3769b28612e595fd440fe55d09
lraphaelcs <lucas.raphael@aluno.ufop.edu.br>
2021-05-28 13:57:37 -0300

    Create ex 7.py
    Apostila São Carlos
    Exercícios Dicionário

---
+++ ex 7.py
@@ -0,0 +1,10 @@
+D = {"Aluno 1": 10,
+    "Aluno 2": 8,
+    "Aluno 3": 6,
+    "Aluno 4": 4,
+    "Aluno 5": 2
+    }
+print ("Notas:" , D)
+L = D.values()
+media = sum(L)/5
+print ("Média das Notas: ", media)
