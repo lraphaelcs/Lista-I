0c381903f03305dc9ccfe1b6c06a9745f5dffc08
lraphaelcs <lucas.raphael@aluno.ufop.edu.br>
2021-05-28 13:58:45 -0300

    Create ex 9.py
    Apostila São Carlos
    Exercícios If

---
+++ ex 9.py
@@ -0,0 +1,10 @@
+n1 = float(input("Nota 1: "))
+n2 = float(input("Nota 2: "))
+media = (n1+n2)/2
+print("Média: ", media)
+if media >6 :
+    print("Aprovado!")
+elif media >=4 :
+    print("Exame.")
+else:
+    print("Reprovado.")
